3845da3a17aa9dbb9b98ec276e51bfc8112cba6a
TareqMonwer <tareqmonwer137@gmail.com>
2022-04-25 22:11:52 +0600

    :sparkles: reduce sql-queries in MillionaireListView

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
-FROM python:3
+FROM python:3.10.4-alpine
+
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
+
 WORKDIR /code
 COPY requirements.txt /code/
+
 RUN pip install -r requirements.txt
+
 COPY . /code/
--- core/views.py
+++ core/views.py
@@ -7,5 +7,8 @@ class MillionaireListView(ListView):
     paginate_by = 16
     template_name = 'core/index.html'
 
+    def get_queryset(self):
+        return self.model.objects.select_related('country', 'company')
+
 
 millionaire_list_view = MillionaireListView.as_view()
